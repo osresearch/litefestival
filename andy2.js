f52d069cc81b944e2b8b80254c28c2ee78072cb1
Trammell Hudson <hudson@trmm.net>
2021-12-05 17:11:22 +0100

    andy2: tweak center position

--- andy2.js
+++ andy2.js
@@ -10,14 +10,14 @@ return function (){
 	
 	background(181,0,91);
 
-	for(let d=15;d<width*.6;d*=1.1){
+	for(let d=15;d<width*.8;d*=1.1){
 		for(let i=0;i<99;i++){
 			let a=t+TAU/99*i+d/(900+sin(t*4)*300) 
 			let x=mid_sin(a)*d
 			let y=mid_sin(a+PI/2)*d/2
 			let s=d/20+(abs(y)-abs(x))/20
 			fill(0)
-			rect(width/2+x-s/2,height/2+y-s/2,s,s)
+			rect(1300+x-s/2,550+y-s/2,s,s)
 		}
 	}
 }
